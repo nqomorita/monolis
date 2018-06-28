dadd52de26ce358b33c2933f159e03b37b3c059b
morita <morita@multi.k.u-tokyo.ac.jp>
2018-06-28 15:06:05 +0900

    Fixed hash tables

--- src/util/hash.f90
+++ src/util/hash.f90
@@ -19,8 +19,8 @@ module mod_monolis_hash
   & 33554393, 67108859, 134217689, 268435399, 536870909, 1073741789, 2147483647/)
 
   type type_monolis_hash_list
-    integer(kind=kint) :: index = 0
-    integer(kind=kint) :: key  = 0
+    integer(kind=kint) :: hash = 0
+    character :: key*27 = "                           "
     integer(kind=kint) :: val  = 0
   end type type_monolis_hash_list
 
@@ -52,13 +52,13 @@ contains
 
   subroutine monolis_hash_finalize()
     implicit none
-    integer(kind=kint) :: i, j
-    type(type_monolis_hash_list), pointer :: list
+    integer(kind=kint) :: i
+    type(type_monolis_hash_list), pointer :: list(:)
     do i = 1, monolis_hash_tree%tree_size
-      do j = 1, monolis_hash_tree%bin(i)%n
-        list => monolis_hash_tree%bin(i)%list(j)
+      if(0 < monolis_hash_tree%bin(i)%n)then
+        list => monolis_hash_tree%bin(i)%list
         if(associated(list)) deallocate(list)
-      enddo
+      endif
     enddo
     deallocate(monolis_hash_tree%bin)
     nullify(list)
@@ -66,19 +66,20 @@ contains
 
   subroutine monolis_hash_get(key, val, is_exist)
     implicit none
-    integer(kind=kint) :: key, hash, index, val
+    integer(kind=kint) :: hash, val
+    character :: key*27
     logical :: is_exist
 
     val = 0
     is_exist = .false.
     call monolis_hash_key(key, hash)
-    index = mod(hash, monolis_hash_tree%tree_size)
-    call monolis_hash_list_get(key, index, val, is_exist)
+    call monolis_hash_list_get(key, hash, val, is_exist)
   end subroutine monolis_hash_get
 
   subroutine monolis_hash_push(key, val, is_pushed, is_exist)
     implicit none
-    integer(kind=kint) :: key, hash, index, val
+    integer(kind=kint) :: hash, val
+    character :: key*27
     logical :: is_exist, is_pushed
 
     is_exist  = .false.
@@ -88,21 +89,22 @@ contains
     endif
 
     call monolis_hash_key(key, hash)
-    index = mod(hash, monolis_hash_tree%tree_size)
-    call monolis_hash_list_get(key, index, val, is_exist)
+    call monolis_hash_list_get(key, hash, val, is_exist)
 
     if(.not. is_exist)then
-      call monolis_hash_list_push(key, index, val)
+      call monolis_hash_list_push(key, hash, val)
+      monolis_hash_tree%n_put = monolis_hash_tree%n_put + 1
       is_pushed = .true.
     endif
   end subroutine monolis_hash_push
 
   subroutine monolis_hash_resize()
     implicit none
-    integer(kind=kint) :: i, j, n, n_put
+    integer(kind=kint) :: i, j, n, hash, val
     integer(kind=kint) :: new_size_id, new_size, old_size
     type(type_monolis_hash_bin), pointer :: new_bin(:), old_bin(:), temp_bin
     type(type_monolis_hash_list), pointer :: list(:)
+    character :: key*27
 
     old_size = monolis_hash_tree%tree_size
     if(monolis_current_hash_size < 22)then
@@ -118,13 +120,12 @@ contains
 
     do i = 1, old_size
       temp_bin => monolis_hash_tree%bin(i)
-      temp_bin%n = old_bin(i)%n
       temp_bin%list => old_bin(i)%list
-      n = old_bin(i)%n
-      do j = 1, n
-        temp_bin%list(j)%index = old_bin(i)%list(j)%index
-        temp_bin%list(j)%key   = old_bin(i)%list(j)%key
-        temp_bin%list(j)%val   = old_bin(i)%list(j)%val
+      do j = 1, old_bin(i)%n
+        hash = temp_bin%list(j)%hash
+        key  = temp_bin%list(j)%key
+        val  = temp_bin%list(j)%val
+        call monolis_hash_list_push(key, hash, val)
       enddo
     enddo
 
@@ -133,15 +134,20 @@ contains
       if(associated(list)) deallocate(list)
     enddo
     deallocate(old_bin)
+    nullify(temp_bin)
+    nullify(old_bin)
+    nullify(new_bin)
   end subroutine monolis_hash_resize
 
-  subroutine monolis_hash_list_get(key, index, val, is_exist)
+  subroutine monolis_hash_list_get(key, hash, val, is_exist)
     implicit none
     integer(kind=kint) :: n, i
-    integer(kind=kint) :: key, index, val
+    integer(kind=kint) :: index, hash, val
+    character :: key*27
     logical :: is_exist
 
     is_exist = .false.
+    call monolis_index_key(hash, index)
     n = monolis_hash_tree%bin(index)%n
     do i = 1, n
       if(monolis_hash_tree%bin(index)%list(i)%key == key)then
@@ -151,26 +157,28 @@ contains
     enddo
   end subroutine monolis_hash_list_get
 
-  subroutine monolis_hash_list_push(key, index, val)
+  subroutine monolis_hash_list_push(key, hash, val)
     implicit none
     integer(kind=kint) :: i, iold, inew
-    integer(kind=kint) :: key, index, val
+    integer(kind=kint) :: index, hash, val
+    character :: key*27
     type(type_monolis_hash_list), pointer :: old_list(:), new_list(:)
 
+    call monolis_index_key(hash, index)
     iold = monolis_hash_tree%bin(index)%n
     old_list => monolis_hash_tree%bin(index)%list
 
     inew = iold + 1
     allocate(new_list(inew))
     do i = 1, iold
-      new_list(i)%index = old_list(i)%index
-      new_list(i)%key   = old_list(i)%key
-      new_list(i)%val   = old_list(i)%val
+      new_list(i)%hash = old_list(i)%hash
+      new_list(i)%key  = old_list(i)%key
+      new_list(i)%val  = old_list(i)%val
     enddo
 
-    new_list(inew)%index = index
-    new_list(inew)%key   = key
-    new_list(inew)%val   = val
+    new_list(inew)%hash = hash
+    new_list(inew)%key  = key
+    new_list(inew)%val  = val
 
     monolis_hash_tree%bin(index)%n = inew
     monolis_hash_tree%bin(index)%list => new_list
@@ -182,8 +190,18 @@ contains
   !> BJD2 hash function
   subroutine monolis_hash_key(key, hash)
     implicit none
-    integer(kind=kint) :: key, hash
-
-    hash = key
+    character :: key*27
+    integer(kind=kint) :: hash, i, t
+    hash = 5381
+    do i = 1, 27
+      t = mod(hash*33, 65536_4)
+      hash = mod(t + iachar(key(i:i)), 65536_4)
+    enddo
   end subroutine monolis_hash_key
+
+  subroutine monolis_index_key(hash, index)
+    implicit none
+    integer(kind=kint) :: hash, index
+    index = mod(hash, monolis_hash_tree%tree_size) + 1
+  end subroutine monolis_index_key
 end module mod_monolis_hash
