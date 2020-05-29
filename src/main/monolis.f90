26d681dd0217544380f520dc1afd6a1184827daa
nqomorita <morita@multi.k.u-tokyo.ac.jp>
2020-05-29 20:21:52 +0900

    Fix main

--- src/main/monolis.f90
+++ src/main/monolis.f90
@@ -8,4 +8,11 @@ module mod_monolis
   use mod_monolis_stdlib
   use mod_monolis_graph
   use mod_monolis_io
+  use mod_monolis_c2d3_shape
+  use mod_monolis_c2d4_shape
+  use mod_monolis_c2d6_shape
+  use mod_monolis_c3d10_shape
+  use mod_monolis_c3d4_shape
+  use mod_monolis_c3d8_shape
+  !use mod_monolis_shape_util
 end module mod_monolis
