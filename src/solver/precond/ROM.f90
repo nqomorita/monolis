c2ef0f5705396dc10508b60d06ccadc30f9dd0c3
Naoki MORITA <nqo.morita@gmail.com>
2021-01-21 17:33:07 +0900

    Add ROM preconditioner

--- src/solver/precond/ROM.f90
+++ src/solver/precond/ROM.f90
@@ -22,18 +22,19 @@ contains
     type(monolis_prm) :: monoPRM
     type(monolis_com) :: monoCOM
     type(monolis_mat) :: monoMAT
-    integer(kint) :: maxiter, NPNDOF
+    integer(kint) :: maxiter, NPNDOF, n_bc
     real(kdouble) :: ths
     logical, allocatable :: is_bc(:)
 
+    allocate(is_bc(monoMAT%NP*monoMAT%NDOF), source = .false.)
+    call get_is_bc(monoPRM, monoCOM, monoMAT, is_bc, n_bc)
+    !write(*,*)"n_bc", n_bc
+
     maxiter = monoMAT%NP*monoMAT%NDOF !min(100, monoMAT%NP*monoMAT%NDOF)
-    n_get_eigen = monoMAT%NP*monoMAT%NDOF !min(10, monoMAT%NP*monoMAT%NDOF)
+    n_get_eigen = monoMAT%NP*monoMAT%NDOF - n_bc !min(10, monoMAT%NP*monoMAT%NDOF)
     ths = 1.0d-4
     allocate(val(n_get_eigen), source = 0.0d0)
     allocate(vec(monoMAT%NP*monoMAT%NDOF, n_get_eigen), source = 0.0d0)
-    allocate(is_bc(monoMAT%NP*monoMAT%NDOF), source = .false.)
-
-    call get_is_bc(monoPRM, monoCOM, monoMAT, is_bc)
 
     monoPRM%precond = monolis_prec_SOR
     monoPRM%show_summary = .false.
@@ -44,8 +45,6 @@ contains
     call interface_monolis_eigen_inverted_standard_lanczos_ &
     & (monoPRM, monoCOM, monoMAT, NPNDOF, n_get_eigen, ths, maxiter, val, vec, is_bc)
 
-write(*,"(1pe12.5)")val(1:n_get_eigen)
-
     monoPRM%show_summary = .true.
     monoPRM%show_time = .true.
     monoPRM%show_iterlog = .true.
@@ -53,12 +52,12 @@ write(*,"(1pe12.5)")val(1:n_get_eigen)
     monoPRM%precond = monolis_prec_ROM
   end subroutine monolis_precond_ROM_setup
 
-  subroutine get_is_bc(monoPRM, monoCOM, monoMAT, is_bc)
+  subroutine get_is_bc(monoPRM, monoCOM, monoMAT, is_bc, n_bc)
     implicit none
     type(monolis_prm) :: monoPRM
     type(monolis_com) :: monoCOM
     type(monolis_mat) :: monoMAT
-    integer(kint) :: i, j, jS, jE, k, l, in, jn, NDOF, NDOF2
+    integer(kint) :: i, j, jS, jE, k, l, in, jn, NDOF, NDOF2, n_bc
     integer(kint), allocatable :: flag(:)
     logical :: is_bc(:)
 
@@ -66,6 +65,7 @@ write(*,"(1pe12.5)")val(1:n_get_eigen)
     NDOF2 = NDOF*NDOF
     allocate(flag(NDOF))
 
+    n_bc = 0
     do i = 1, monoMAT%N
       jS = monoMAT%index(i-1) + 1
       jE = monoMAT%index(i  )
@@ -80,7 +80,10 @@ write(*,"(1pe12.5)")val(1:n_get_eigen)
         enddo
       enddo
       do j = 1, NDOF
-        if(flag(j) == 1) is_bc(NDOF*(i-1)+j) = .true.
+        if(flag(j) == 1)then
+          is_bc(NDOF*(i-1)+j) = .true.
+          n_bc = n_bc + 1
+        endif
       enddo
     enddo
   end subroutine get_is_bc
@@ -94,17 +97,14 @@ write(*,"(1pe12.5)")val(1:n_get_eigen)
     real(kdouble) :: X(:), Y(:), phi
     real(kdouble), allocatable :: coef(:)
 
-!write(*,*)"monolis_precond_ROM_apply start"
-!write(*,"(1p3e12.5)")X
-
     allocate(coef(n_get_eigen), source = 0.0d0)
 
     do i = 1, n_get_eigen
       phi = 0.0d0
       do j = 1, monoMAT%NP*monoMAT%NDOF
-        phi = phi + vec(j,i)*X(i)
+        phi = phi + vec(j,i)*X(j)
       enddo
-      coef(i) = phi*val(i)
+      coef(i) = phi/val(i)
     enddo
 
     Y = 0.0d0
@@ -113,9 +113,6 @@ write(*,"(1pe12.5)")val(1:n_get_eigen)
         Y(j) = Y(j) + vec(j,i)*coef(i)
       enddo
     enddo
-
-!write(*,*)"monolis_precond_ROM_apply end"
-!write(*,"(1p3e12.5)")Y
   end subroutine monolis_precond_ROM_apply
 
   subroutine monolis_precond_ROM_clear(monoPRM, monoCOM, monoMAT)
