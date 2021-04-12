114aadf3a92e155e3ce43ef79dbec6ecab8a3aeb
Naoki MORITA <nqo.morita@gmail.com>
2021-04-13 02:42:47 +0900

    Add scalapack

--- install_lib.sh
+++ install_lib.sh
@@ -3,17 +3,49 @@
 git submodule update --init --recursive
 BASE_DIR=$(pwd)
 
-#> for METIS
-cd submodule/METIS
-make config prefix=$BASE_DIR
-make install
-cd ../..
+for OPT in "$@"
+do
+    case $OPT in
+        MUMPS)
+            FLAG_MUMPS=1
+            ;;
+        METIS)
+            FLAG_METIS=1
+            ;;
+        SCALAPACK)
+            FLAG_SCALAPACK=1
+            ;;
+    esac
+done
 
-#> for MUMPS
-cd submodule/mumps
-mkdir build
-cd build
-cmake -Darith=d -DCMAKE_INSTALL_PREFIX=$BASE_DIR ..
-make -j
-make install
+if [ "$#" == 0 ]; then
+    FLAG_MUMPS=1
+    FLAG_METIS=1
+    FLAG_SCALAPACK=1
+fi
 
+if [ "$FLAG_SCALAPACK" ]; then
+    cd submodule/scalapack
+    mkdir build
+    cd build
+    cmake -DCMAKE_INSTALL_PREFIX=$BASE_DIR ..
+    make -j
+    make install
+    cd ../..
+fi
+
+if [ "$FLAG_MUMPS" ]; then
+    cd submodule/mumps
+    mkdir build
+    cd build
+    cmake -Darith=d -DCMAKE_INSTALL_PREFIX=$BASE_DIR ..
+    make -j
+    make install
+fi
+
+if [ "$FLAG_METIS" ]; then
+    cd submodule/METIS
+    make config prefix=$BASE_DIR
+    make install
+    cd ../..
+fi
