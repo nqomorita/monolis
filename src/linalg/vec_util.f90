0228c0c6e9a5deee3f8f253209e66124f680b560
Naoki MORITA <nqo.morita@gmail.com>
2023-02-28 17:40:32 +0900

    Add mod_monolis_vec_util

--- src/linalg/vec_util.f90
+++ src/linalg/vec_util.f90
@@ -1,3 +1,4 @@
+!> ベクトル演算関数群
 module mod_monolis_vec_util
   use mod_monolis_utils
   implicit none
@@ -5,10 +6,48 @@ module mod_monolis_vec_util
 contains
 
   !> @ingroup dev_linalg
+  !> ベクトル配列コピー（整数型）
+  subroutine monolis_vec_copy_I(n, ndof, X, Y)
+    implicit none
+    !> 自由度数
+    integer(kint) :: n
+    !> ブロックサイズ
+    integer(kint) :: ndof
+    !> ベクトル 1
+    integer(kint) :: X(:)
+    !> ベクトル 2
+    integer(kint) :: Y(:)
+    integer(kint) :: i
+
+#ifdef DEBUG
+    call monolis_std_debug_log_header("monolis_vec_copy_I")
+#endif
+
+!$omp parallel default(none) &
+!$omp & shared(X, Y) &
+!$omp & firstprivate(n, ndof) &
+!$omp & private(i)
+!$omp do
+    do i = 1, n * ndof
+      Y(i) = X(i)
+    enddo
+!$omp end do
+!$omp end parallel
+  end subroutine monolis_vec_copy_I
+
+  !> @ingroup dev_linalg
+  !> ベクトル配列コピー（実数型）
   subroutine monolis_vec_copy_R(n, ndof, X, Y)
     implicit none
-    integer(kint) :: i, n, ndof
-    real(kdouble) :: X(:), Y(:)
+    !> 自由度数
+    integer(kint) :: n
+    !> ブロックサイズ
+    integer(kint) :: ndof
+    !> ベクトル 1
+    real(kdouble) :: X(:)
+    !> ベクトル 2
+    real(kdouble) :: Y(:)
+    integer(kint) :: i
 
 #ifdef DEBUG
     call monolis_std_debug_log_header("monolis_vec_copy_R")
@@ -27,14 +66,55 @@ contains
   end subroutine monolis_vec_copy_R
 
   !> @ingroup dev_linalg
-  subroutine monolis_vec_AXPY(n, ndof, alpha, X, Y, Z)
+  !> ベクトル配列コピー（複素数型）
+  subroutine monolis_vec_copy_C(n, ndof, X, Y)
     implicit none
-    integer(kint) :: i, n, ndof
-    real(kdouble) :: alpha
-    real(kdouble) :: X(:), Y(:), Z(:)
+    !> 自由度数
+    integer(kint) :: n
+    !> ブロックサイズ
+    integer(kint) :: ndof
+    !> ベクトル 1
+    complex(kdouble) :: X(:)
+    !> ベクトル 2
+    complex(kdouble) :: Y(:)
+    integer(kint) :: i
 
 #ifdef DEBUG
-    call monolis_std_debug_log_header("monolis_vec_AXPY")
+    call monolis_std_debug_log_header("monolis_vec_copy_C")
+#endif
+
+!$omp parallel default(none) &
+!$omp & shared(X, Y) &
+!$omp & firstprivate(n, ndof) &
+!$omp & private(i)
+!$omp do
+    do i = 1, n * ndof
+      Y(i) = X(i)
+    enddo
+!$omp end do
+!$omp end parallel
+  end subroutine monolis_vec_copy_C
+
+  !> @ingroup dev_linalg
+  !> ベクトル和 $z = \alpha * x + y$ （整数型）
+  subroutine monolis_vec_AXPY_I(n, ndof, alpha, X, Y, Z)
+    implicit none
+    !> 自由度数
+    integer(kint) :: n
+    !> ブロックサイズ
+    integer(kint) :: ndof
+    !> ベクトル 1
+    integer(kint) :: alpha
+    !> ベクトル 1
+    integer(kint) :: X(:)
+    !> ベクトル 2
+    integer(kint) :: Y(:)
+    !> ベクトル 3
+    integer(kint) :: Z(:)
+    integer(kint) :: i
+
+#ifdef DEBUG
+    call monolis_std_debug_log_header("monolis_vec_AXPY_I")
 #endif
 
 !$omp parallel default(none) &
@@ -47,6 +127,73 @@ contains
     enddo
 !$omp end do
 !$omp end parallel
-  end subroutine monolis_vec_AXPY
+  end subroutine monolis_vec_AXPY_I
 
+  !> @ingroup dev_linalg
+  !> ベクトル和 $z = \alpha * x + y$ （実数型）
+  subroutine monolis_vec_AXPY_R(n, ndof, alpha, X, Y, Z)
+    implicit none
+    !> 自由度数
+    integer(kint) :: n
+    !> ブロックサイズ
+    integer(kint) :: ndof
+    !> ベクトル 1
+    real(kdouble) :: alpha
+    !> ベクトル 1
+    real(kdouble) :: X(:)
+    !> ベクトル 2
+    real(kdouble) :: Y(:)
+    !> ベクトル 3
+    real(kdouble) :: Z(:)
+    integer(kint) :: i
+
+#ifdef DEBUG
+    call monolis_std_debug_log_header("monolis_vec_AXPY_R")
+#endif
+
+!$omp parallel default(none) &
+!$omp & shared(X, Y, Z) &
+!$omp & firstprivate(n, ndof, alpha) &
+!$omp & private(i)
+!$omp do
+    do i = 1, n * ndof
+      Z(i) = alpha*X(i) + Y(i)
+    enddo
+!$omp end do
+!$omp end parallel
+  end subroutine monolis_vec_AXPY_R
+
+  !> @ingroup dev_linalg
+  !> ベクトル和 $z = \alpha * x + y$ （複素数型）
+  subroutine monolis_vec_AXPY_C(n, ndof, alpha, X, Y, Z)
+    implicit none
+    !> 自由度数
+    integer(kint) :: n
+    !> ブロックサイズ
+    integer(kint) :: ndof
+    !> ベクトル 1
+    complex(kdouble) :: alpha
+    !> ベクトル 1
+    complex(kdouble) :: X(:)
+    !> ベクトル 2
+    complex(kdouble) :: Y(:)
+    !> ベクトル 3
+    complex(kdouble) :: Z(:)
+    integer(kint) :: i
+
+#ifdef DEBUG
+    call monolis_std_debug_log_header("monolis_vec_AXPY_C")
+#endif
+
+!$omp parallel default(none) &
+!$omp & shared(X, Y, Z) &
+!$omp & firstprivate(n, ndof, alpha) &
+!$omp & private(i)
+!$omp do
+    do i = 1, n * ndof
+      Z(i) = alpha*X(i) + Y(i)
+    enddo
+!$omp end do
+!$omp end parallel
+  end subroutine monolis_vec_AXPY_C
 end module mod_monolis_vec_util
