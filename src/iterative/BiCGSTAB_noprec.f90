ec202050289acf04679acba29b209100f070a56c
takumi308 <omuraisusan@gmail.com>
2023-04-15 19:02:12 +0900

    Fix BiCGSTAB_noprec.f90

--- src/iterative/BiCGSTAB_noprec.f90
+++ src/iterative/BiCGSTAB_noprec.f90
@@ -16,14 +16,14 @@ contains
   !> BiCGStab 法（前処理なし）
   subroutine monolis_solver_BiCGSTAB_noprec(monoPRM, monoCOM, monoMAT, monoPREC)
     implicit none
-    !> パラメータ構造体
-    type(monolis_prm) :: monoPRM
-    !> 通信テーブル構造体
-    type(monolis_com) :: monoCOM
-    !> 行列構造体
-    type(monolis_mat), target :: monoMAT
-    !> 前処理構造体
-    type(monolis_mat) :: monoPREC
+    !> [in,out] パラメータ構造体
+    type(monolis_prm), intent(inout) :: monoPRM
+    !> [in] 通信テーブル構造体
+    type(monolis_com), intent(in) :: monoCOM
+    !> [in] 行列構造体
+    type(monolis_mat), target, intent(in) :: monoMAT
+    !> [in] 前処理構造体
+    type(monolis_mat), intent(in) :: monoPREC
     integer(kint) :: N, NP, NDOF, NNDOF
     integer(kint) :: i, iter, iter_RR
     real(kdouble) :: alpha, beta, rho, rho1, c2, omega
@@ -122,5 +122,4 @@ contains
     call monolis_dealloc_R_1d(T )
     call monolis_dealloc_R_1d(V )
   end subroutine monolis_solver_BiCGSTAB_noprec
-
 end module mod_monolis_solver_BiCGSTAB_noprec
