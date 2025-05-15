503abef1fe52ec346a118a5d516be6778ff2d70a
Naoki MORITA <nmorita@kz.tsukuba.ac.jp>
2025-05-16 00:29:15 +0900

    Fix sor V

--- src/prec/sor/sor_V.f90
+++ src/prec/sor/sor_V.f90
@@ -164,106 +164,125 @@ contains
     type(monolis_mat), target, intent(in) :: monoMAT
     !> [in] 前処理構造体
     type(monolis_mat), target, intent(in) :: monoPREC
-    integer(kint) :: i, j, jE, jS, jn, k, l, N, NP, NDOF, NDOF2
+    integer(kint) :: i, j, jE, jS, in, jn, kn, ln, k, l, NDOF
+    integer(kint) :: n1, n2, nz
+    integer(kint) :: NNDOF, NPNDOF, NDOF_MAX
     integer(kint), pointer :: index(:)
     integer(kint), pointer :: item(:)
     real(kdouble) :: X(:), Y(:)
     real(kdouble), pointer :: A(:), ALU(:)
     real(kdouble), allocatable :: XT(:), YT(:), ST(:)
 
-    N =  monoMAT%N
-    NP = monoMAT%NP
-    NDOF = monoMAT%NDOF
-    NDOF2 = NDOF*NDOF
     A => monoMAT%R%A
     ALU => monoPREC%R%D
     index => monoMAT%CSR%index
     item => monoMAT%CSR%item
 
-    do i = 1, NP*NDOF
+    NDOF_MAX = maxval(monoMAT%n_dof_list)
+
+    call monolis_get_vec_size(monoMAT%N, monoMAT%NP, monoMAT%NDOF, &
+      monoMAT%n_dof_index, NNDOF, NPNDOF)
+
+    do i = 1, NNDOF
       Y(i) = X(i)
     enddo
 
-    call monolis_alloc_R_1d(XT, NDOF)
-    call monolis_alloc_R_1d(YT, NDOF)
-    call monolis_alloc_R_1d(ST, NDOF)
+    call monolis_alloc_R_1d(XT, NDOF_MAX)
+    call monolis_alloc_R_1d(YT, NDOF_MAX)
+    call monolis_alloc_R_1d(ST, NDOF_MAX)
 
-    do i = 1, N
-      do j = 1, NDOF
-        ST(j) = Y(NDOF*(i-1)+j)
+    nz = 0
+    do i = 1, monoMAT%N
+      n1 = monoMAT%n_dof_list(i)
+      in = monoMAT%n_dof_index(i)
+      do j = 1, n1
+        ST(j) = Y(in + j)
       enddo
       jS = index(i) + 1
       jE = index(i + 1)
       do j = jS, jE
         jn = item(j)
+        n2 = monoMAT%n_dof_list(jn)
         if(jn < i)then
-          do k = 1, NDOF
-            XT(k) = Y(NDOF*(jn-1)+k)
+          kn = monoMAT%n_dof_index(jn)
+          do k = 1, n2
+            XT(k) = Y(kn + k)
           enddo
-          do k = 1, NDOF
-            do l = 1, NDOF
-              ST(k) = ST(k) - A(NDOF2*(j-1)+NDOF*(k-1)+l) * XT(l)
+          ln = monoMAT%n_dof_index2(j)
+          do k = 1, n1
+            do l = 1, n2
+              ST(k) = ST(k) - A(ln + n2*(k-1) + l)*XT(l)
             enddo
           enddo
         endif
       enddo
 
-      do j = 1, NDOF
+      do j = 1, n1
         XT(j) = ST(j)
       enddo
-      do j = 2, NDOF
+      do j = 2, n1
         do k = 1, j-1
-          XT(j) = XT(j) - ALU(NDOF2*(i-1) + NDOF*(j-1) + k)*XT(k)
+          XT(j) = XT(j) - ALU(nz + n1*(j-1) + k)*XT(k)
         enddo
       enddo
-      do j = NDOF, 1, -1
-        do k = NDOF, j+1, -1
-          XT(j) = XT(j) - ALU(NDOF2*(i-1) + NDOF*(j-1) + k)*XT(k)
+      do j = n1, 1, -1
+        do k = n1, j+1, -1
+          XT(j) = XT(j) - ALU(nz + n1*(j-1) + k)*XT(k)
         enddo
-        XT(j) = ALU(NDOF2*(i-1) + (NDOF+1)*(j-1) + 1)*XT(j)
+        XT(j) = ALU(nz + (n1+1)*(j-1) + 1)*XT(j)
       enddo
-      do k = 1, NDOF
-        Y(NDOF*(i-1)+k) = XT(k)
+      do k = 1, n1
+        Y(in + k) = XT(k)
       enddo
+      nz = nz + n1*n1
     enddo
 
-    do i = N, 1, -1
-      do j = 1, NDOF
+    in = monoMAT%n_dof_list(monoMAT%N)
+    nz = monoMAT%n_dof_index2(monoMAT%N) - in*in
+    do i = monoMAT%N, 1, -1
+      n1 = monoMAT%n_dof_list(i)
+      do j = 1, n1
         ST(j) = 0.0d0
       enddo
       jS = index(i) + 1
       jE = index(i + 1)
       do j = jE, jS, -1
         jn = item(j)
+        n2 = monoMAT%n_dof_list(jn)
         if(i < jn)then
-          do k = 1, NDOF
-            XT(k) = Y(NDOF*(jn-1)+k)
+          kn = monoMAT%n_dof_index(jn)
+          do k = 1, n2
+            XT(k) = Y(kn + k)
           enddo
-          do k = 1, NDOF
-            do l = 1, NDOF
-              ST(k) = ST(k) + A(NDOF2*(j-1)+(k-1)*NDOF+l)*XT(l)
+          ln = monoMAT%n_dof_index2(j)
+          do k = 1, n1
+            do l = 1, n2
+              ST(k) = ST(k) + A(ln + n2*(k-1) + l)*XT(l)
             enddo
           enddo
         endif
       enddo
 
-      do j = 1, NDOF
+      do j = 1, n1
         XT(j) = ST(j)
       enddo
-      do j = 2, NDOF
+      do j = 2, n1
         do k = 1, j-1
-          XT(j) = XT(j) - ALU(NDOF2*(i-1) + NDOF*(j-1) + k)*XT(k)
+          XT(j) = XT(j) - ALU(nz + n1*(j-1) + k)*XT(k)
         enddo
       enddo
-      do j = NDOF, 1, -1
-        do k = NDOF, j+1, -1
-          XT(j) = XT(j) - ALU(NDOF2*(i-1) + NDOF*(j-1) + k)*XT(k)
+      do j = n1, 1, -1
+        do k = n1, j+1, -1
+          XT(j) = XT(j) - ALU(nz + n1*(j-1) + k)*XT(k)
         enddo
-        XT(j) = ALU(NDOF2*(i-1) + (NDOF+1)*(j-1) + 1)*XT(j)
+        XT(j) = ALU(nz + (n1+1)*(j-1) + 1)*XT(j)
       enddo
-      do k = 1, NDOF
-        Y(NDOF*(i-1)+k) = Y(NDOF*(i-1)+k) - XT(k)
+      in = monoMAT%n_dof_index(i)
+      do k = 1, n1
+        Y(in + k) = Y(in + k) - XT(k)
       enddo
+      in = monoMAT%n_dof_list(i - 1)
+      nz = nz - in*in
     enddo
 
     deallocate(XT)
