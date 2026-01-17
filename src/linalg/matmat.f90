1d281cd59d76656c12cc12e99d3fe3ca96b276bd
Naoki MORITA <nmorita@kz.tsukuba.ac.jp>
2026-01-17 15:05:12 +0900

    Fix monolis_matmat_value_nn

--- src/linalg/matmat.f90
+++ src/linalg/matmat.f90
@@ -8,7 +8,7 @@ contains
 
   !> @ingroup linalg
   !> ベクトル内積
-  subroutine monolis_matmat_local(monoCOM, A, B, C, tspmv, tcomm)
+  subroutine monolis_matmat_local(monoCOM, A, B, C, is_CSC_A, is_CSC_B, tspmv, tcomm)
     implicit none
     !> [in] 通信テーブル構造体
     type(monolis_com), intent(in) :: monoCOM
@@ -18,6 +18,10 @@ contains
     type(monolis_mat), intent(in) :: B
     !> [in] 行列構造体
     type(monolis_mat), intent(inout) :: C
+    !>
+    logical :: is_CSC_A
+    !>
+    logical :: is_CSC_B
     !> [in,out] 計算時間
     real(kdouble), intent(inout) :: tspmv
     !> [in,out] 通信時間
@@ -69,7 +73,7 @@ contains
     !> [in] 計算点が持つ自由度
     integer(kint), intent(in) :: NDOF
     integer(kint) :: i, j, k, jA, jB, jS, jE, col_A, col_B, nz, NP
-    integer(kint), allocatable :: mask(:), row_C(:)
+    integer(kint), allocatable :: row_C(:)
     logical, allocatable :: is_nonzero(:)
 
 #ifdef DEBUG
@@ -83,7 +87,6 @@ contains
     C%NP = A%NP
     C%NDOF = NDOF
 
-    call monolis_alloc_I_1d(mask, NP)
     call monolis_alloc_L_1d(is_nonzero, NP)
     call monolis_alloc_I_1d(row_C, NP)
     call monolis_palloc_I_1d(C%CSR%index, NP + 1)
@@ -155,7 +158,6 @@ contains
       enddo
     enddo
 
-    call monolis_dealloc_I_1d(mask)
     call monolis_dealloc_L_1d(is_nonzero)
     call monolis_dealloc_I_1d(row_C)
   end subroutine monolis_matmat_symbolic
@@ -182,19 +184,11 @@ contains
 
     NP = A%NP
     NDOF2 = NDOF * NDOF
-
-    ! C の行列値配列のメモリ確保と初期化
     nz = C%CSR%index(NP + 1)
-    call monolis_pdealloc_R_1d(C%R%A)
     call monolis_palloc_R_1d(C%R%A, NDOF2 * nz)
-    C%R%A = 0.0d0
-
-    ! 一時配列の確保（ブロック行列の積計算用）
     call monolis_alloc_R_2d(temp, NDOF, NDOF)
 
-    ! 各行について処理
     do i = 1, NP
-      ! C の i 行の各非零要素について
       jS = C%CSR%index(i) + 1
       jE = C%CSR%index(i + 1)
       
@@ -202,27 +196,26 @@ contains
         col_B = C%CSR%item(jC)
         temp = 0.0d0
 
-        ! C(i, col_B) = Σ_k A(i, k) * B(k, col_B) を計算
-        ! A の i 行の各非零要素について
+        !# C(i, col_B) = Σ_k A(i, k) * B(k, col_B) を計算
+        !# A の i 行の各非零要素
         jS = A%CSR%index(i) + 1
         jE = A%CSR%index(i + 1)
         do jA = jS, jE
           col_A = A%CSR%item(jA)
 
-          ! B(col_A, col_B) が非零かチェック
+          !# B(col_A, col_B) が非零かチェック
           jS = B%CSR%index(col_A) + 1
           jE = B%CSR%index(col_A + 1)
           do jB = jS, jE
             if(B%CSR%item(jB) == col_B)then
-              ! ブロック行列の積を計算: temp += A(i,col_A) * B(col_A,col_B)
+              !# ブロック行列の積を計算: temp += A(i,col_A) * B(col_A,col_B)
               kn_A = NDOF2 * (jA - 1)
               kn_B = NDOF2 * (jB - 1)
 
-              ! temp = temp + A_block * B_block (NDOF x NDOF のブロック)
               do k = 1, NDOF
                 do l = 1, NDOF
                   do m = 1, NDOF
-                    temp(k, l) = temp(k, l) + A%R%A(kn_A + NDOF*(k-1) + m) * B%R%A(kn_B + NDOF*(m-1) + l)
+                    temp(l,k) = temp(l,k) + A%R%A(kn_A + NDOF*(k-1) + m) * B%R%A(kn_B + NDOF*(m-1) + l)
                   enddo
                 enddo
               enddo
@@ -231,17 +224,16 @@ contains
           enddo
         enddo
 
-        ! 計算結果を C に格納
+        !# 計算結果を C に格納
         kn_C = NDOF2 * (jC - 1)
         do k = 1, NDOF
           do l = 1, NDOF
-            C%R%A(kn_C + NDOF*(k-1) + l) = temp(k, l)
+            C%R%A(kn_C + NDOF*(k-1) + l) = temp(l,k)
           enddo
         enddo
       enddo
     enddo
 
-    ! 一時配列の解放
     call monolis_dealloc_R_2d(temp)
   end subroutine monolis_matmat_value_nn
 end module mod_monolis_matmat
