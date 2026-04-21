ec1f3e1338a1ad5a750e8ca7d08a5578d269ca29
Naoki MORITA <nmorita@kz.tsukuba.ac.jp>
2026-04-21 16:19:24 +0900

    Fix monolis_matvec_c_test.c

--- wrapper_test/linalg/monolis_matvec_c_test.c
+++ wrapper_test/linalg/monolis_matvec_c_test.c
@@ -503,12 +503,104 @@ void monolis_matvec_c_test_C33(){
   monolis_finalize(&mat);
 }
 
+void monolis_matvec_c_test_R_V(){
+  MONOLIS mat;
+  MONOLIS_COM com;
+  int i;
+  int n_node;
+  int n_elem;
+  int** elem;
+  int* n_dof_list;
+  double rx[7];
+  double ry[7];
+
+  monolis_std_log_string("monolis_matvec_c_test_R_V");
+
+  n_node = 5;
+  n_elem = 4;
+
+  elem = monolis_alloc_I_2d(elem, n_elem, 2);
+
+  elem[0][0] = 0; elem[0][1] = 1;
+  elem[1][0] = 1; elem[1][1] = 2;
+  elem[2][0] = 2; elem[2][1] = 3;
+  elem[3][0] = 3; elem[3][1] = 4;
+
+  n_dof_list = monolis_alloc_I_1d(n_dof_list, n_node);
+  n_dof_list[0] = 1;
+  n_dof_list[1] = 2;
+  n_dof_list[2] = 1;
+  n_dof_list[3] = 2;
+  n_dof_list[4] = 1;
+
+  monolis_initialize(&mat);
+  monolis_com_initialize_by_self(&com);
+
+  monolis_get_nonzero_pattern_by_simple_mesh_V_R(&mat, n_node, 2, n_dof_list, n_elem, elem);
+
+  /* node 0 (dof 1): flat index 0 */
+  monolis_add_scalar_to_sparse_matrix_R(&mat, 0, 0, 0, 0, 2.0);
+  monolis_add_scalar_to_sparse_matrix_R(&mat, 0, 1, 0, 0, 1.0);
+
+  /* node 1 (dof 2): flat indices 1, 2 */
+  monolis_add_scalar_to_sparse_matrix_R(&mat, 1, 0, 0, 0, 1.0);
+  monolis_add_scalar_to_sparse_matrix_R(&mat, 1, 1, 0, 0, 2.0);
+  monolis_add_scalar_to_sparse_matrix_R(&mat, 1, 1, 1, 1, 3.0);
+  monolis_add_scalar_to_sparse_matrix_R(&mat, 1, 2, 0, 0, 3.0);
+
+  /* node 2 (dof 1): flat index 3 */
+  monolis_add_scalar_to_sparse_matrix_R(&mat, 2, 1, 0, 0, 1.0);
+  monolis_add_scalar_to_sparse_matrix_R(&mat, 2, 2, 0, 0, 2.0);
+  monolis_add_scalar_to_sparse_matrix_R(&mat, 2, 3, 0, 0, 4.0);
+
+  /* node 3 (dof 2): flat indices 4, 5 */
+  monolis_add_scalar_to_sparse_matrix_R(&mat, 3, 2, 0, 0, 1.0);
+  monolis_add_scalar_to_sparse_matrix_R(&mat, 3, 3, 0, 0, 2.0);
+  monolis_add_scalar_to_sparse_matrix_R(&mat, 3, 3, 1, 1, 4.0);
+  monolis_add_scalar_to_sparse_matrix_R(&mat, 3, 4, 0, 0, 5.0);
+
+  /* node 4 (dof 1): flat index 6 */
+  monolis_add_scalar_to_sparse_matrix_R(&mat, 4, 3, 0, 0, 1.0);
+  monolis_add_scalar_to_sparse_matrix_R(&mat, 4, 4, 0, 0, 2.0);
+
+  for(i = 0; i < 7; ++i){
+    rx[i] = 1.0 + i;
+  }
+
+  for(i = 0; i < 7; ++i){
+    ry[i] = 0.0;
+  }
+
+  monolis_matvec_product_R(&mat, &com, rx, ry);
+
+  /* expected: ry = A * rx (rx = [1,2,3,4,5,6,7])
+   * dense matrix (7x7), flat dof layout: [0][1,2][3][4,5][6]
+   * ry[0] = 2*1 + 1*2                   =  4
+   * ry[1] = 1*1 + 2*2 + 3*4             = 17
+   * ry[2] = 3*3                         =  9
+   * ry[3] = 1*2 + 2*4 + 4*5             = 30
+   * ry[4] = 1*4 + 2*5 + 5*7             = 49
+   * ry[5] = 4*6                         = 24
+   * ry[6] = 1*5 + 2*7                   = 19
+   */
+  monolis_test_check_eq_R1("monolis_matvec_c_test_R_V", ry[0],  4.0);
+  monolis_test_check_eq_R1("monolis_matvec_c_test_R_V", ry[1], 17.0);
+  monolis_test_check_eq_R1("monolis_matvec_c_test_R_V", ry[2],  9.0);
+  monolis_test_check_eq_R1("monolis_matvec_c_test_R_V", ry[3], 30.0);
+  monolis_test_check_eq_R1("monolis_matvec_c_test_R_V", ry[4], 49.0);
+  monolis_test_check_eq_R1("monolis_matvec_c_test_R_V", ry[5], 24.0);
+  monolis_test_check_eq_R1("monolis_matvec_c_test_R_V", ry[6], 19.0);
+
+  monolis_finalize(&mat);
+}
+
 void monolis_matvec_c_test(){
-  if(monolis_mpi_get_global_comm_size() == 1) return
+  if(monolis_mpi_get_global_comm_size() != 1) return
 
   monolis_matvec_c_test_R11();
   monolis_matvec_c_test_R22();
   monolis_matvec_c_test_R33();
+  monolis_matvec_c_test_R_V();
 
   monolis_matvec_c_test_C11();
   monolis_matvec_c_test_C22();
