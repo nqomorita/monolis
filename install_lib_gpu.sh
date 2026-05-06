013d0b552c06a21b22f84ef32005ad55f09eea55
morita <nqo.morita@gmail.com>
2026-05-06 22:36:45 +0900

    Add install_lib_gpu.sh

---
+++ install_lib_gpu.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+git submodule update --init --recursive
+BASE_DIR=$(pwd)
+
+#> scalapack
+cd submodule/scalapack
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=$BASE_DIR -DCMAKE_C_FLAGS="-Wno-implicit-function-declaration" -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ..
+make -j
+make install
+cd ../../..
+
+#> METIS
+cd submodule/METIS
+make config prefix=$BASE_DIR
+make install
+cd ../..
+#make config i64=1 prefix=$BASE_DIR
+
+#> ParMETIS
+cd submodule/ParMETIS
+make config prefix=$BASE_DIR
+#make config i64=1 prefix=$BASE_DIR
+make install
+cd ../..
+
+#> MUMPS
+cd submodule/mumps
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=$BASE_DIR ..
+make -j 
+make install
+cd ../../..
+
+#> monolis_utils
+cd submodule/monolis_utils/
+make clean
+make FLAGS=GPU_GNU
+cd ../..
+
+#> gedatsu
+cd submodule/gedatsu/
+make clean
+make FLAGS=SUBMODULE
+cd ../..
+
+#> gedatsu
+cd submodule/ggtools/
+make clean
+make FLAGS=SUBMODULE
+cd ../..
