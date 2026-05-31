589f61d201ddaa4bef9465ef6a9d2514e0572b22
morita <nqo.morita@gmail.com>
2026-05-31 20:27:04 +0900

    Add test

--- src_test/monolis_test.f90
+++ src_test/monolis_test.f90
@@ -10,6 +10,7 @@ program monolis_test
   use mod_monolis_spmat_handler_util_test
   use mod_monolis_spmat_handler_test
   use mod_monolis_spmat_convert_sym_test
+  use mod_monolis_spmat_convert_dia_test
   use mod_monolis_vec_util_test
   use mod_monolis_linalg_test
   use mod_monolis_converge_test
@@ -54,6 +55,7 @@ program monolis_test
   call monolis_spmat_handler_util_test()
   call monolis_spmat_handler_test()
   call monolis_spmat_convert_sym_test()
+  call monolis_spmat_convert_dia_test()
 
   call monolis_vec_util_test()
   call monolis_linalg_test()
