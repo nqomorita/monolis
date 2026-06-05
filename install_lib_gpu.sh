abd5714552598922869e508f532c99cbf3d30729
morita <nqo.morita@gmail.com>
2026-06-05 13:43:13 +0900

    Fix install_lib_gpu.sh

--- install_lib_gpu.sh
+++ install_lib_gpu.sh
@@ -44,11 +44,11 @@ cd ../..
 #> gedatsu
 cd submodule/gedatsu/
 make clean
-make FLAGS=SUBMODULE
+make FLAGS=SUBMODULE,GPU_GNU
 cd ../..
 
 #> gedatsu
 cd submodule/ggtools/
 make clean
-make FLAGS=SUBMODULE
+make FLAGS=SUBMODULE,GPU_GNU
 cd ../..
